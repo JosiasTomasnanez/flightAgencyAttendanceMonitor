88ee8f643887c06650e4863df95736fba83ffd2e
Nicolas Piñera <nicolaspinera@gmail.com>
2026-01-05 22:50:18 -0300

    Definicion de Constantes

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,35 @@ import java.util.ArrayList;
 
 /**
  * Clase principal que gestiona la ejecución del programa de la agencia de
- * vuelos. Esta clase se encarga de la interacción con el usuario, 
- * la selección de la política, la inicialización de la matriz de incidencia, 
+ * vuelos. Esta clase se encarga de la interacción con el usuario,
+ * la selección de la política, la inicialización de la matriz de incidencia,
  * el marcado inicial y la creación y ejecución de los hilos que simulan las
  * diferentes tareas de la agencia.
  */
 public class Main {
+  public static final int CANTIDAD_HILOS_AGENTE_1 = 1;
+  public static final int CANTIDAD_HILOS_AGENTE_2 = 1;
+  public static final int CANTIDAD_HILOS_CANCELACION = 1;
+  public static final int CANTIDAD_HILOS_CONFIRMACION = 1;
+  public static final int CANTIDAD_HILOS_GEN_CLIENTES = 5;
+
   public static void main(String[] args) {
 
     int numero = 1;
     PoliticaAgenciaVuelo politica = null;
 
-    try{
-        politica = new PoliticaAgenciaVuelo(numero);
+    try {
+      politica = new PoliticaAgenciaVuelo(numero);
     } catch (PoliticaInexistenteException pie) {
-        System.out.println("El numero ingresado no corresponde a una politica valida");
-      }
-    
-    // Se genera la instancia de la red de petri, la misma genera su matriz y su marcado inicial
+      System.out.println("El numero ingresado no corresponde a una politica valida");
+    }
+
+    // Se genera la instancia de la red de petri, la misma genera su matriz y su
+    // marcado inicial
     RedDePetri redDePetri = new RedDePetri();
 
-    // La red de petri es la que sabe que transiciones son temporales y que intervalo tiene
+    // La red de petri es la que sabe que transiciones son temporales y que
+    // intervalo tiene
     Monitor.getInstance(redDePetri, politica, redDePetri.getAlfayBeta());
 
     {
@@ -35,25 +43,31 @@ public class Main {
     ArrayList<Thread> hilos = new ArrayList<>();
 
     // 1 hilo por agente
-    for (int i = 0; i < 1; i++)
-      hilos.add(
-          factory.newThread(new AtencionAgente(NumeroDeAgente.AGENTE1, Monitor.getInstance())));
+    for (int i = 0; i < CANTIDAD_HILOS_AGENTE_1; i++) {
+      hilos.add(factory.newThread(new AtencionAgente(NumeroDeAgente.AGENTE1, Monitor.getInstance())));
+    }
 
-    for (int i = 0; i < 1; i++)
-      hilos.add(
-          factory.newThread(new AtencionAgente(NumeroDeAgente.AGENTE2, Monitor.getInstance())));
+    for (int i = 0; i < CANTIDAD_HILOS_AGENTE_2; i++){
+      hilos.add(factory.newThread(new AtencionAgente(NumeroDeAgente.AGENTE2, Monitor.getInstance())));
+    }
+      
 
     // 1 Hilo encargado de la cancelacion
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < CANTIDAD_HILOS_CANCELACION; i++){
       hilos.add(factory.newThread(new Cancelacion(Monitor.getInstance())));
+    }
+      
 
     // 1 Hilo encargado de la confirmacion y pago
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < CANTIDAD_HILOS_CONFIRMACION; i++){
       hilos.add(factory.newThread(new ConfirmacionYPago(Monitor.getInstance())));
+    }
+      
 
     // 5 hilos encargados de la generacion y entrada de clientes
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < CANTIDAD_HILOS_GEN_CLIENTES; i++){
       hilos.add(factory.newThread(new EntradaDeClientes(Monitor.getInstance())));
+    }
 
     // Hilo encargado del Log
     hilos.add(factory.newThread(new Log(redDePetri)));
